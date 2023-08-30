804154321620484523c758b073cc5161ab995c35
Kevin <staffordkevinjames@gmail.com>
2023-08-30 15:10:02 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+const mainMain = document.getElementById("main");
+mainMain.remove();
+
+const newHeader = document.createElement('h1');
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Kevin is the champion";
